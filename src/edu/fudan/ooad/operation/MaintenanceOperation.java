550846421369da47d4c911aadaed4f934b2f41f5
Kaiyuan <dogin2006@hotmail.com>
2016-01-07 21:32:54 +0800

    Refactor: add comment and upgrade Criteria

--- src/edu/fudan/ooad/operation/MaintenanceOperation.java
+++ src/edu/fudan/ooad/operation/MaintenanceOperation.java
@@ -8,6 +8,7 @@ import edu.fudan.ooad.provider.HibernateManager;
 import edu.fudan.ooad.util.DateUtils;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
 import java.util.ArrayList;
@@ -23,6 +24,13 @@ public class MaintenanceOperation {
 
     private static final long MILL_SEC_DAY = 60 * 60 * 24 * 1000;
 
+    /**
+     * 得到date日期后days天内需要做的维修任务
+     *
+     * @param date 日期
+     * @param days 时间间隔
+     * @return 要做的任务
+     */
     public static List<Task> getDaysTask(Date date, int days) {
         List<Task> taskList = new ArrayList<>();
         Session session = null;
@@ -61,6 +69,12 @@ public class MaintenanceOperation {
         return taskList;
     }
 
+    /**
+     * 获取date日期后十天内要做的任务
+     *
+     * @param date 日期
+     * @return 要做的任务
+     */
     public static List<Task> getTenDaysTask(Date date) {
         return getDaysTask(date, 10);
     }
@@ -78,50 +92,64 @@ public class MaintenanceOperation {
         return getMonthTask(DateUtils.getCalendar().get(Calendar.YEAR), month);
     }
 
+    /**
+     * 获取当月要做的任务
+     *
+     * @param year  年
+     * @param month 月
+     * @return 要做的任务
+     */
     public static List<Task> getMonthTask(int year, int month) {
         Calendar calendar = DateUtils.getCalendar(year, month, 1);
         return getDaysTask(calendar.getTime(), calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
     }
 
+    /**
+     * 获得某设备总维护时间
+     *
+     * @param equipmentId 设备Id
+     * @return 维护时间
+     */
     public static int getTotalMaintenanceTime(String equipmentId) {
         Session session = null;
-        int total = 0;
         try {
             session = HibernateManager.getSession();
-            List<Record> records = session.createCriteria(Record.class)
+            Long sum = (Long) session.createCriteria(Record.class)
                     .add(Restrictions.eq("equipmentId", equipmentId))
-                    .list();
-            for (Record record :
-                    records) {
-                total += record.getDuration();
-            }
+                    .setProjection(Projections.sum("duration"))
+                    .uniqueResult();
+            return sum == null ? 0 : sum.intValue();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             HibernateManager.closeSession(session);
         }
-        return total;
+        return 0;
     }
 
+    /**
+     * 获得某设备某计划的总维护时间
+     *
+     * @param equipment 设备
+     * @param planId    计划Id
+     * @return 总维护时间
+     */
     public static int getTotalMaintenanceTime(Equipment equipment, String planId) {
         Session session = null;
-        int total = 0;
         try {
             session = HibernateManager.getSession();
-            List<Record> records = session.createCriteria(Record.class)
+            Long sum = (Long) session.createCriteria(Record.class)
                     .add(Restrictions.eq("equipmentId", equipment.getId()))
                     .add(Restrictions.eq("planId", planId))
-                    .list();
-            for (Record record :
-                    records) {
-                total += record.getDuration();
-            }
+                    .setProjection(Projections.sum("duration"))
+                    .uniqueResult();
+            return sum == null ? 0 : sum.intValue();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             HibernateManager.closeSession(session);
         }
-        return total;
+        return 0;
     }
 
 }
