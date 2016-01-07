02ded2a4315429e8dfe731aaeb126bde3740003e
Kaiyuan <dogin2006@hotmail.com>
2016-01-07 22:28:06 +0800

    final commit

--- src/edu/fudan/ooad/entity/IEntity.java
+++ src/edu/fudan/ooad/entity/IEntity.java
@@ -2,11 +2,23 @@ package edu.fudan.ooad.entity;
 
 /**
  * Created by Kaiyuan on 2016/1/7.
+ * <p>
+ * base interface for all model class
  */
 public interface IEntity {
 
+    /**
+     * get object id
+     *
+     * @return the object id
+     */
     String getId();
 
+    /**
+     * set object id
+     *
+     * @param id the object id
+     */
     void setId(String id);
 
 }
